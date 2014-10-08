9a169dc377b848f2d99f2cfdb51aa1e438624163
Zachary Radtka <zachary.radtka@gmail.com>
2014-10-08 15:25:25 +0200

    ZER - Print things out very nicely

--- Cubes/src/main/java/com/threeSixtyT/Cube.java
+++ Cubes/src/main/java/com/threeSixtyT/Cube.java
@@ -7,56 +7,61 @@ import com.threeSixtyT.Side.Edge;
 
 public class Cube {
 
+	/** The number of sides in a cube */
 	private static final int NUM_SIDES = 6;
-	private List<Side> sides;
+	
+	/** A list of sides representing the cube */
+	private List<Side> cube;
 	
 	
 	public Cube(String[][] sides) {
 		
 		// Initialize the list to be 
-		this.sides = new ArrayList<Side>(NUM_SIDES);
+		this.cube = new ArrayList<Side>(NUM_SIDES);
 		
 		for (String[] currSide: sides) {
-			this.sides.add(new Side(currSide));
+			this.cube.add(new Side(currSide));
 		}
 	}
 	
 	public void addSides(String[][] sides) {
 		// Initialize the list to be 
-		this.sides = new ArrayList<Side>(NUM_SIDES);
+		this.cube = new ArrayList<Side>(NUM_SIDES);
 		
 		for (String[] currSide: sides) {
-			this.sides.add(new Side(currSide));
+			this.cube.add(new Side(currSide));
 		}
 	}
 	
-	public void displayCube() {
-		
-		// Loop through the list of sides and display them
-		for (Side currSide: this.sides) {
+	public void display() {
+		this.display(this.cube);
+	}
+	
+	public void display(List<Side> cube) {
+		for (Side currSide: cube) {
 			currSide.display();
 			System.out.println();
 		}
 	}
 
+
 	
 	public void solve() {
 		
-		List<Side> potentialSolution = new ArrayList<Side>(NUM_SIDES);
+		List<Side> solution = new ArrayList<Side>(NUM_SIDES);
 		List<Side> remainingSides = new ArrayList<Side>(NUM_SIDES);
 		
-		remainingSides.addAll(sides);
+		remainingSides.addAll(cube);
 				
-		findValidSide(potentialSolution, remainingSides, 0, 0, 0, false);
+		findValidSide(solution, remainingSides, 0, 0, 0, false);
 	}
 
-	private void findValidSide(List<Side> potentialSolution, List<Side> remainingSides, int currPosition, int index, int numRotation, boolean hasFlipped) {
+	private void findValidSide(List<Side> solution, List<Side> remainingSides, int currPosition, int index, int numRotation, boolean hasFlipped) {
 		
 		if (currPosition == NUM_SIDES) {
 			System.out.println("Solution Found");
-			for (Side currSide: potentialSolution) {
-				currSide.display();
-			}
+			this.displaySolution(solution);
+//			this.display(solution);
 		}
 		
 		if (index >= remainingSides.size()) {
@@ -65,12 +70,13 @@ public class Cube {
 			
 		
 		// If a match 
-		if (sidesMatch(currPosition, potentialSolution, remainingSides.get(index))) {
+		if (sidesMatch(currPosition, solution, remainingSides.get(index))) {
 			
-			potentialSolution.add(remainingSides.remove(index));
-			findValidSide(potentialSolution, remainingSides, currPosition+1, 0, 0, false);			
-			remainingSides.add(index, potentialSolution.remove(currPosition));
+			solution.add(remainingSides.remove(index));
+			findValidSide(solution, remainingSides, currPosition+1, 0, 0, false);			
+			remainingSides.add(index, solution.remove(currPosition));
 		}
+		
 		
 		// If no match rotate the shape
 		if (numRotation < remainingSides.get(index).getNumSupportedRotations()) {
@@ -78,17 +84,16 @@ public class Cube {
 			// Rotate the shape to its right
 			remainingSides.get(index).rotateRight();
 			
-			findValidSide(potentialSolution, remainingSides, currPosition, index, numRotation+1, hasFlipped);		
+			findValidSide(solution, remainingSides, currPosition, index, numRotation+1, hasFlipped);		
 			
 			// Rotate back to the original position
 			remainingSides.get(index).rotateRight();
 
 		} else if (!hasFlipped && remainingSides.get(index).isFlipAllowed()) {
-			
 			// Flip
 			remainingSides.get(index).flip();
 			
-			findValidSide(potentialSolution, remainingSides, currPosition, index, 0, true);			
+			findValidSide(solution, remainingSides, currPosition, index, 0, true);			
 
 			// Flip back to original orientation
 			remainingSides.get(index).flip();
@@ -97,36 +102,58 @@ public class Cube {
 			
 			// Every position except for the first position uses the index
 			// increase the index
-			findValidSide(potentialSolution, remainingSides, currPosition, index+1, 0, false);			
+			findValidSide(solution, remainingSides, currPosition, index+1, 0, false);			
 		}
 	}
 	
 
-	private boolean sidesMatch(int currPosition, List<Side> potentialSolution, Side currSide) {
+	private boolean sidesMatch(int currPosition, List<Side> solution, Side currSide) {
 		
 		if (currPosition == 0) {
 			// No comparison needs to be done
 			return true;
 		} else if (currPosition == 1 || currPosition == 2) {
-			return potentialSolution.get(currPosition-1).
-					matchesEdge(Edge.BOTTOM,currSide.getEdge(Edge.TOP));
+			return solution.get(currPosition-1).
+					matches(Edge.BOTTOM,currSide.getEdge(Edge.TOP));
 		} else if (currPosition == 3) {
-			return currSide.matchesEdge(Edge.TOP, potentialSolution.get(2).getEdge(Edge.BOTTOM))
-				&& currSide.matchesEdge(Edge.BOTTOM, potentialSolution.get(0).getEdge(Edge.TOP));
+			return currSide.matches(Edge.TOP, solution.get(2).getEdge(Edge.BOTTOM))
+				&& currSide.matches(Edge.BOTTOM, solution.get(0).getEdge(Edge.TOP));
 		} else if (currPosition == 4) {
-			return currSide.matchesEdge(Edge.RIGHT, potentialSolution.get(3).getEdge(Edge.LEFT))
-					&& currSide.matchesEdge(Edge.TOP, potentialSolution.get(2).getEdge(Edge.LEFT))
-					&& currSide.matchesEdge(Edge.LEFT, potentialSolution.get(1).getEdge(Edge.LEFT), true)
-					&& currSide.matchesEdge(Edge.BOTTOM, potentialSolution.get(0).getEdge(Edge.LEFT), true);
+			return currSide.matches(Edge.RIGHT, solution.get(3).getEdge(Edge.LEFT))
+					&& currSide.matches(Edge.TOP, solution.get(2).getEdge(Edge.LEFT))
+					&& currSide.matches(Edge.LEFT, solution.get(1).getEdge(Edge.LEFT), true)
+					&& currSide.matches(Edge.BOTTOM, solution.get(0).getEdge(Edge.LEFT), true);
 		} else if (currPosition == 5) {
-			return currSide.matchesEdge(Edge.LEFT, potentialSolution.get(3).getEdge(Edge.RIGHT))
-					&& currSide.matchesEdge(Edge.TOP, potentialSolution.get(2).getEdge(Edge.RIGHT))
-					&& currSide.matchesEdge(Edge.RIGHT, potentialSolution.get(1).getEdge(Edge.RIGHT), true)
-					&& currSide.matchesEdge(Edge.BOTTOM, potentialSolution.get(0).getEdge(Edge.RIGHT), true);
+			return currSide.matches(Edge.LEFT, solution.get(3).getEdge(Edge.RIGHT))
+					&& currSide.matches(Edge.TOP, solution.get(2).getEdge(Edge.RIGHT))
+					&& currSide.matches(Edge.RIGHT, solution.get(1).getEdge(Edge.RIGHT), true)
+					&& currSide.matches(Edge.BOTTOM, solution.get(0).getEdge(Edge.RIGHT), true);
 		}
 		
 		return false;
 	}
 	
+	
+	private void displaySolution(List<Side> solution) {
+		int dimension = solution.get(0).getDimension();	
+		
+		// Display the first three sides
+		for(int i=0; i < 3; i++) {
+			Side currSide = solution.get(i);
+			
+			if (i < 3) {				
+				for (int j=0; j< dimension; j++) {
+					System.out.format("%" + 2*dimension + "s\n", currSide.getRow(j));
+				}
+			}
+		}
+		
+		// Display the last three sides
+		for (int i=0; i < dimension; i++) {
+			System.out.format("%s%s%s\n", solution.get(4).getRow(i), solution.get(3).getRow(i), solution.get(5).getRow(i));
+		}
+		
+		System.out.println();
+	}
 	
 }
--- Cubes/src/main/java/com/threeSixtyT/Side.java
+++ Cubes/src/main/java/com/threeSixtyT/Side.java
@@ -29,16 +29,16 @@ public class Side {
 		this.side = new Boolean[this.dimension][this.dimension];
 
 		// Copy the passed in array to this array
-		for (int x=0; x < this.dimension; x++) {
-			for (int y=0; y < side[x].length(); y++) {
-				this.side[x][y] = (side[x].charAt(y) == '1') ? true: false;
+		for (int y=0; y < this.dimension; y++) {
+			for (int x=0; x < side[y].length(); x++) {
+				this.side[y][x] = (side[y].charAt(x) == '1') ? true: false;
 			}
 		}
 		
 		// Determine if the shape has a reflection to limit the number of rotations
-		if (this.equalsEdge(Edge.TOP, this.getEdge(Edge.RIGHT)) 
+		if (this.edgeEquals(Edge.TOP, this.getEdge(Edge.RIGHT)) 
 				&& this.equalsEdge(Edge.RIGHT, this.getEdge(Edge.BOTTOM), true)
-				&& this.equalsEdge(Edge.BOTTOM, this.getEdge(Edge.LEFT)) 
+				&& this.edgeEquals(Edge.BOTTOM, this.getEdge(Edge.LEFT)) 
 				&& this.equalsEdge(Edge.LEFT, this.getEdge(Edge.TOP), true)) {
 			this.numSupportedRotations = 0;
 		} else if (this.equalsEdge(Edge.TOP, this.getEdge(Edge.BOTTOM), true) 
@@ -54,13 +54,28 @@ public class Side {
 
 	}
 	
+	
+	public int getDimension() {
+		return this.dimension;
+	}
 
+	public String getRow(int rowNum) {
+		
+		StringBuilder row = new StringBuilder();
+
+		for (int x=0; x < this.dimension; x++) {
+			row.append(this.side[rowNum][x] ? "o" : " ");
+		}
+
+		return row.toString();
+	}
+	
 	public void display() {
 		
 		// Print the values
-		for (int x=0; x < this.dimension; x++) {
-			for (int y=0; y < this.dimension; y++) {
-				System.out.print((this.side[x][y]) ? "o" : " ");
+		for (int y=0; y < this.dimension; y++) {
+			for (int x=0; x < this.dimension; x++) {
+				System.out.print((this.side[y][x]) ? "o" : " ");
 			}
 			System.out.print("\n");
 		}
@@ -73,16 +88,16 @@ public class Side {
 		Boolean[][] rotatedEdge = new Boolean[this.dimension][this.dimension];
 		
 		// Print the values
-		for (int x=0; x < this.dimension; x++) {
-			for (int y=0; y < this.dimension; y++) {
-				rotatedEdge[x][y] = this.side[this.dimension - y -1][x];				
+		for (int y=0; y < this.dimension; y++) {
+			for (int x=0; x < this.dimension; x++) {
+				rotatedEdge[y][x] = this.side[this.dimension - x -1][y];				
 			}
 		}
 		
 		// Copy new side into this side
-		for (int x=0; x < this.dimension; x++) {
-			for (int y=0; y < this.dimension; y++) {
-				this.side[x][y] = rotatedEdge[x][y];				
+		for (int y=0; y < this.dimension; y++) {
+			for (int x=0; x < this.dimension; x++) {
+				this.side[y][x] = rotatedEdge[y][x];				
 			}
 		}
 	}
@@ -92,7 +107,7 @@ public class Side {
 
 		// Invert the value
 		for (int x=0; x < this.dimension; x++) {
-			invertEdge(this.side[x]);
+			reverseEdge(this.side[x]);
 		}
 	}
 	
@@ -106,17 +121,17 @@ public class Side {
 		return this.flipAllowed;
 	}
 	
-	public boolean equalsEdge(Edge edgeType, Boolean[] edgeToCompare) {
+	public boolean edgeEquals(Edge edgeType, Boolean[] edgeToCompare) {
 		return this.equalsEdge(edgeType, edgeToCompare, false);
 	}
 	
 	
-	private boolean equalsEdge(Edge edgeType, Boolean[] edgeToCompare, boolean invert) {
+	public boolean equalsEdge(Edge edgeType, Boolean[] edgeToCompare, boolean invert) {
 		
 		Boolean[] thisSideEdge = this.getEdge(edgeType);
 		
 		if (invert) {
-			thisSideEdge = invertEdge(thisSideEdge);
+			thisSideEdge = reverseEdge(thisSideEdge);
 		}
 		
 		for (int i=0; i< this.dimension; i++) {
@@ -129,15 +144,15 @@ public class Side {
 	}
 
 
-	public boolean matchesEdge(Edge edgeType, Boolean[] edgeToCompare) {
-		return this.matchesEdge(edgeType, edgeToCompare, false);
+	public boolean matches(Edge edgeType, Boolean[] edgeToCompare) {
+		return this.matches(edgeType, edgeToCompare, false);
 	}
 	
-	public boolean matchesEdge(Edge edgeType, Boolean[] edgeToCompare, Boolean invert) {
+	public boolean matches(Edge edgeType, Boolean[] edgeToCompare, Boolean invert) {
 		Boolean[] thisSideEdge = this.getEdge(edgeType);
 		
 		if (invert) {
-			thisSideEdge = invertEdge(thisSideEdge);
+			thisSideEdge = reverseEdge(thisSideEdge);
 		}
 		
 		for (int i=0; i< this.dimension; i++) {
@@ -176,7 +191,7 @@ public class Side {
 		}
 	}
 	
-	private Boolean[] invertEdge(Boolean[] edge) {
+	private Boolean[] reverseEdge(Boolean[] edge) {
 
 		for (int i=0; i < edge.length/2; i++) {
 			Boolean tmp = edge[edge.length - (1+i)];
@@ -201,6 +216,8 @@ public class Side {
 		
 		return true;
 	}
+	
+	
 	
 	private boolean containsHorizontalReflection() {
 		
